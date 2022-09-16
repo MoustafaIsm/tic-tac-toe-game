57eb40e93a469964d4a843442eda07f0478a49ea
Moustafa <moustafaism013@hotmail.com>
2022-09-16 16:34:47 +0300

    Prepared my variables

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// Variable
+const turn = document.getElementById("turn");
+const positions = document.getElementsByClassName("position");
+const result = document.getElementById("result");
+const restartBtn = document.getElementById("restart-btn");
+
+// Eventlisteners functions
+
+
+// Event listeners
+
+
+
+// Hlper functions
+
